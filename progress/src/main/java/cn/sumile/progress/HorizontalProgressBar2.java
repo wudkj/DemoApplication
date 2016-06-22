c2d40af75e28603700000fccd80d9e64bcc4965e
sumile <wudkj@163.com>
2016-06-22 18:46:12 +0800

    添加progress2

--- progress/src/main/java/cn/sumile/progress/HorizontalProgressBar2.java
+++ progress/src/main/java/cn/sumile/progress/HorizontalProgressBar2.java
@@ -9,9 +9,9 @@ import android.util.TypedValue;
 import android.widget.ProgressBar;
 
 /**
- * Created by zhangxiaokang on 16/6/20.
+ * Created by Administrator on 2016/6/22.
  */
-public class HorizontalProgressBar extends ProgressBar {
+public class HorizontalProgressBar2 extends ProgressBar {
     private static final int DEFAULT_TEXT_SIZE = 10;
     private static final int DEFAULT_TEXT_COLOR = 0xffFC00D1;
     private static final int DEFAULT_Color_UNREACH = 0xFFD3D6DA;
@@ -21,94 +21,98 @@ public class HorizontalProgressBar extends ProgressBar {
     private static final int DEFAULT_TEXT_OFFSET = 10;
 
     private int mTextSize = spToPx(DEFAULT_TEXT_SIZE);
+
     private int mTextColor = DEFAULT_TEXT_COLOR;
+
     private int mUnreachHeight = dpToPx(DEFAULT_HEIGHT_UNREACH);
     private int mUnreachColor = DEFAULT_Color_UNREACH;
     private int mReachHeight = dpToPx(DEFAULT_HEIGHT_REACH);
     private int mReachColor = DEFAULT_COLOR_REACH;
-    private int mTextOffset = dpToPx(DEFAULT_TEXT_OFFSET);
+    private int mTextOffeset = dpToPx(DEFAULT_TEXT_OFFSET);
 
     private Paint mPaint = new Paint();
 
-    private int mRealWidth;
+    private int mRealWidth = 0;
 
-    public HorizontalProgressBar(Context context) {
+    public HorizontalProgressBar2(Context context) {
         this(context, null);
     }
 
-    public HorizontalProgressBar(Context context, AttributeSet attrs) {
+    public HorizontalProgressBar2(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public HorizontalProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+    public HorizontalProgressBar2(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        obtainStyleAttrs(attrs);
+        obtainAttributeValues(attrs);
         mPaint.setTextSize(mTextSize);
     }
 
-    /**
-     * 获得自定义参数
-     *
-     * @param attrs
-     */
-    private void obtainStyleAttrs(AttributeSet attrs) {
-        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.HorizontalProgressBar);
-        mTextColor = ta.getColor(R.styleable.HorizontalProgressBar_progress_text_color, mTextColor);
-        mTextSize = (int) ta.getDimension(R.styleable.HorizontalProgressBar_progress_text_size, mTextSize);
-        mReachColor = ta.getColor(R.styleable.HorizontalProgressBar_progress_reach_color, mReachColor);
-        mReachHeight = (int) ta.getDimension(R.styleable.HorizontalProgressBar_progress_reach_height, mReachHeight);
-        mUnreachColor = ta.getColor(R.styleable.HorizontalProgressBar_progress_unreach_color, mUnreachColor);
-        mUnreachHeight = (int) ta.getDimension(R.styleable.HorizontalProgressBar_progress_unreach_height, mUnreachHeight);
+
+    private int dpToPx(int defaultDp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, defaultDp, getResources().getDisplayMetrics());
+    }
+
+    private void obtainAttributeValues(AttributeSet attrs) {
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.Ho2);
+        mTextColor = ta.getColor(R.styleable.Ho2_progress_text_color, mTextColor);
+        mTextSize = (int) ta.getDimension(R.styleable.Ho2_progress_text_size, mTextSize);
+
+        mReachColor = ta.getColor(R.styleable.Ho2_progress_reach_color, mReachColor);
+        mReachHeight = (int) ta.getDimension(R.styleable.Ho2_progress_reach_height, mReachHeight);
+
+        mUnreachColor = ta.getColor(R.styleable.Ho2_progress_unreach_color, mUnreachColor);
+        mUnreachHeight = (int) ta.getDimension(R.styleable.Ho2_progress_unreach_height, mUnreachHeight);
+
+        mTextOffeset = (int) ta.getDimension(R.styleable.Ho2_progress_text_offset, mTextOffeset);
         ta.recycle();
     }
 
     @Override
     protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        int height = measureHeight(heightMeasureSpec);
-        setMeasuredDimension(widthSize, height);
+        int resultHeight = measureHeight(heightMeasureSpec);
+        setMeasuredDimension(widthSize, resultHeight);
 
         mRealWidth = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
     }
 
     @Override
     protected synchronized void onDraw(Canvas canvas) {
+        //init paint and canvas
         canvas.save();
-        canvas.translate(getPaddingLeft(), getHeight() / 2);
-
-        boolean noNeedUnreach = false;
+        canvas.translate(getPaddingLeft(), getMeasuredHeight() / 2);//这里我使用了getMeasureHeight
+        boolean needToDrawUnreach = true;
         //draw reach bar
         float radio = getProgress() * 1.0f / getMax();
         float progressX = radio * mRealWidth;
         String text = getProgress() + "%";
-        float textWidth = mPaint.measureText(text);
-        if (textWidth + progressX >= mRealWidth) {
+        int textWidth = (int) mPaint.measureText(text);
+        if (progressX + textWidth >= mRealWidth) {
             progressX = mRealWidth - textWidth;
-            noNeedUnreach = true;
+            needToDrawUnreach = false;
         }
-        float endX = progressX - mTextOffset / 2;
+        int endX = (int) (progressX - mTextOffeset / 2);
         if (endX > 0) {
             mPaint.setColor(mReachColor);
             mPaint.setStrokeWidth(mReachHeight);
             canvas.drawLine(0, 0, endX, 0, mPaint);
         }
+
         //draw text
         mPaint.setColor(mTextColor);
         mPaint.setStrokeWidth(mTextSize);
-        int y = (int) (Math.abs(mPaint.descent() + mPaint.ascent()) / 2) ;
+        int y = (int) -((mPaint.descent() + mPaint.ascent())/2);
         canvas.drawText(text, progressX, y, mPaint);
-
         //draw unreach bar
-        if (!noNeedUnreach) {
-            float start = progressX + mTextOffset / 2 + textWidth;
+        if (needToDrawUnreach) {
             mPaint.setColor(mUnreachColor);
             mPaint.setStrokeWidth(mUnreachHeight);
-            canvas.drawLine(Math.min(start, mRealWidth), 0, mRealWidth, 0, mPaint);
+            canvas.drawLine(endX + textWidth + mTextOffeset, 0, mRealWidth, 0, mPaint);
         }
-
+        //restore canvas
         canvas.restore();
-
     }
 
     private int measureHeight(int heightMeasureSpec) {
@@ -118,20 +122,17 @@ public class HorizontalProgressBar extends ProgressBar {
         if (mode == MeasureSpec.EXACTLY) {
             result = size;
         } else {
-            int textSize = (int) (mPaint.descent() - mPaint.ascent());
-            result = getPaddingTop() + getPaddingBottom() + Math.max(Math.max(mUnreachHeight, mReachHeight), Math.abs(textSize));
+            int textHeight = (int) (mPaint.descent() - mPaint.ascent());
+            int maxHeight = getPaddingTop() + getPaddingBottom() + Math.max(Math.max(mUnreachHeight, mReachHeight), Math.abs(textHeight));
+            result = maxHeight;
             if (mode == MeasureSpec.AT_MOST) {
-                result = Math.min(size, result);
+                result = Math.min(size, maxHeight);
             }
         }
         return result;
     }
 
-    private int spToPx(int spValue) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spValue, getResources().getDisplayMetrics());
-    }
-
-    private int dpToPx(int dpValue) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, getResources().getDisplayMetrics());
+    private int spToPx(int defaultSp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, defaultSp, getResources().getDisplayMetrics());
     }
 }
--- progress/src/main/java/cn/sumile/progress/MainActivity.java
+++ progress/src/main/java/cn/sumile/progress/MainActivity.java
@@ -1,19 +1,50 @@
 package cn.sumile.progress;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActivityChooserView;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private TextView tv;
+    private ProgressBar progress;
+    private int i = 0;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (i > 100) {
+                return;
+            }
+            progress.setProgress(msg.arg1);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progress = (ProgressBar) findViewById(R.id.progress);
+        new Thread() {
+            @Override
+            public void run() {
+                while (i <= 100) {
+                    Message msg = new Message();
+                    msg.arg1 = i;
+                    mHandler.dispatchMessage(msg);
+                    i++;
+                    try {
+                        sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 }
